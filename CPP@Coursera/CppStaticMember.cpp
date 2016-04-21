2dc64c2086a313c1ffc70c503c445cf3e48fda5d
Lei <lei.yan@rwth-aachen.de>
2016-04-21 20:52:37 +0200

    figure out the CMake issue, lowercase/biggercase bug -_-#, FINISH static member learning issue, better learn CMake later XD

--- CPP@Coursera/CppStaticMember.cpp
+++ CPP@Coursera/CppStaticMember.cpp
@@ -12,14 +12,25 @@
 #include <iostream>
 #include "CRectangle.h"
 
+/*predeclaration, just like an global variable,
+ *static member variable is sort of pseudo global variable XD
+ */
 int	CRectangle::totalNumber = 0;
 int CRectangle::totalArea = 0;
 
+void foo(CRectangle r)
+{
+	std::cout << "foo called" << std::endl;
+}
+
 int main()
 {
-
 	CRectangle r1(1);
 	CRectangle r2 = r1;
-	return 0;
+	CRectangle r3(2);
+	double i = 5.0;
+	r3 = i;
+	foo(r3);
 	CRectangle::printTotal();
+	return 0;
 }
--- CPP@Coursera/include/CRectangle.h
+++ CPP@Coursera/include/CRectangle.h
@@ -1,41 +1,48 @@
 /*Class for static member properties test*/
 
-
-
 class CRectangle
 {
 private:
 	int area;
+	int whatever;
 	static int totalNumber;
 	static int totalArea;
 public:
-	CRectangle(int i);
+	CRectangle(int i, int j=0);
 	CRectangle(CRectangle& r);
+	// CRectangle(double a){ area = a; whatever = 0; }
 	~CRectangle();
 	static void printTotal();
 };
 
-CRectangle::CRectangle(int i)
+CRectangle::CRectangle(int i, int j)
 {
 	area = i;
+	whatever = j;
 	totalNumber++;
 	totalArea += area;
+	std::cout << "cons called" << std::endl;
+	std::cout << "total number + :" << totalNumber << std::endl;
 }
 
 CRectangle::CRectangle(CRectangle& r)
 {
 	area = r.area;
+	whatever = r.whatever;
 	totalNumber++;
 	totalArea += area;
+	std::cout << "copy cons called" << std::endl;
+	std::cout << "total number + :" << totalNumber << std::endl;
 }
 
 CRectangle::~CRectangle()
 {
 	totalNumber--;
 	totalArea -= area;
+	std::cout << "total number - :" << totalNumber << std::endl;
 }
 
-static void CRectangle::printTotal()
+void CRectangle::printTotal()
 {
 	std::cout << "total number:" << totalNumber << std::endl;
 	std::cout << "total area:" << totalArea << std::endl;
