83cb183a762c5412815efe49dab603cac974cd07
Lei <lei.yan@rwth-aachen.de>
2016-06-10 17:24:06 +0200

    small update to operator overloading stuff -- Cpp@Coursera-week4-lecture3

--- CPP@Coursera/CppOperatorOL.cpp
+++ CPP@Coursera/CppOperatorOL.cpp
@@ -8,6 +8,14 @@
  *             as the object already serves as an operand, A+B ----> A.operator+(B)
  * 2. Overloading of "=", sort of important stuff !!!
  *
+ *
+ * updated on June 10. 2016: (Example shown @ Cpp@Coursera -- week 4 -- lecture 3)
+ *
+ * b = c + 5 is simply to realize through overloading of "+" as member function,
+ * But how to realize such operations? --> b = 5 + c, we need a access to the private members.
+ * And we can not use member function to overload operator here, so a normal function as friend
+ * of the class works.
+ *
  */
 
 #include <iostream>
@@ -22,6 +30,7 @@ public:
 	void CcomplexPrint () { std::cout << real << "+" << image << "i" << std::endl; }
 	// friend is awesome !!!
 	friend Ccomplex operator- (const Ccomplex& c1, const Ccomplex& c2);
+	friend Ccomplex operator- (const double o1, const Ccomplex& o2);
 };
 
 Ccomplex Ccomplex::operator+ (const Ccomplex& c)
@@ -39,13 +48,20 @@ Ccomplex operator- (const Ccomplex& c1, const Ccomplex& c2)
     return Ccomplex(c1.real-c2.real, c1.image-c2.image);
 }
 
+Ccomplex operator- (const double o1, const Ccomplex& o2)
+{
+	return Ccomplex(o1-o2.real, -o2.image);
+}
+
 int main()
 {
 	Ccomplex c1(3,4), c2(4,5);
 	Ccomplex c3 = c1 + c2;
 	c2 = c1 - c3;
+	Ccomplex c4 = 5 - c3;
 	c1.CcomplexPrint();
 	c2.CcomplexPrint();
 	c3.CcomplexPrint();
+	c4.CcomplexPrint();
 	return 0;
 }
